0b4066c0464d5e82924fd2c6a35a6c3a9706c047
Bipul Singh <bipulkumarvats154@gmail.com>
2026-05-18 12:38:15 +0530

    Seed production catalog when empty

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { ensureAdmin } from "./utils/ensureAdmin.js";
+import { seedCatalogIfEmpty } from "./utils/seedCatalog.js";
 
 const port = process.env.PORT || 5000;
 
 connectDB()
+  .then(seedCatalogIfEmpty)
   .then(ensureAdmin)
   .then(() => {
     app.listen(port, () => console.log(`API running on port ${port}`));
