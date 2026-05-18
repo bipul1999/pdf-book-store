dc871dcc93019818f6394dfe0291a8b749de1f61
Bipul Singh <bipulkumarvats154@gmail.com>
2026-05-18 14:02:16 +0530

    Force production MongoDB database name

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 export async function connectDB() {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI is missing");
   mongoose.set("strictQuery", true);
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME || "pdf-book-store" });
   console.log("MongoDB connected");
 }
